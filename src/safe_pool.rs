5dd10a104a9aedd22cc8180113c0e724035a9044
Alex Babkin <ababkin@Alexs-MacBook-Pro.local>
2024-07-16 15:53:41 +0200

    use cluster version of deadpool primitives so its compatible with clusters

--- src/safe_pool.rs
+++ src/safe_pool.rs
@@ -1,21 +1,18 @@
-use anyhow::{Result, Error};
-use std::env;
-use url::Url;
+use anyhow::{Error, Result};
 use tokio::sync::Mutex;
-use deadpool_redis::{redis::cmd, Config as PoolConfig, Connection, Manager, Pool, Runtime};
-use tracing::{error, debug, warn, info};
+use tracing::{debug, error, warn};
+use deadpool_redis::cluster::{Connection, Pool, Config, Runtime};
 
 
-#[derive(Debug)]
 pub struct SafePool {
     pool: Mutex<Option<Pool>>,
     url: String,
 }
 
 impl SafePool {
-    pub fn new(url: String) -> SafePool { 
+    pub fn new(url: String) -> SafePool {
         SafePool {
-            pool: Mutex::new(None), 
+            pool: Mutex::new(None),
             url,
         }
     }
@@ -28,7 +25,7 @@ impl SafePool {
     pub async fn ensure(&self) {
         loop {
             debug!("Trying to connect to Redis...");
-            match create(&self.url) {
+            match create(&self.url).await {
                 Ok(pool) => {
                     let mut locked = self.pool.lock().await;
                     *locked = Some(pool);
@@ -49,18 +46,24 @@ impl SafePool {
                 let lock = self.pool.lock().await;
                 if let Some(pool) = &*lock {
                     return Ok(pool.get().await?)
-                } 
+                }
             }
             warn!("Redis connection lost, attempting to reconnect...");
             self.ensure().await;
-            // tokio::time::sleep(tokio::time::Duration::from_secs(1)).await; // Wait before retrying
         }
     }
-
 }
 
-fn create(url: &str) -> Result<Pool, Error> {
-    let cfg = deadpool_redis::Config::from_url(url);
+async fn create(url: &str) -> Result<Pool, Error> {
+    let cfg = Config::from_urls(vec![url.to_string()]);
     let pool = cfg.create_pool(Some(Runtime::Tokio1))?;
     Ok(pool)
 }
+
+
+
+// fn create_cluster_pool(urls: &[String]) -> Result<Pool, Box<dyn std::error::Error>> {
+//     let mut cfg = Config::from_urls(redis_urls);
+//     let pool = cfg.create_pool(Some(Runtime::Tokio1)).unwrap();
+
+// }
