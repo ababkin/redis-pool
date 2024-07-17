0ef350b1cb7ba848ee7b9b463cfda4e789dffe8b
Alex Babkin <ababkin@Alexs-MacBook-Pro.local>
2024-07-17 09:54:09 +0200

    disable safe pool for now

--- src/safe_pool.rs
+++ src/safe_pool.rs
@@ -1,59 +1,78 @@
 use anyhow::{Error, Result};
 use tokio::sync::RwLock;
-use tracing::{debug, error, warn};
+use tracing::{ debug, error, warn, instrument };
 use deadpool_redis::cluster::{Connection, Pool, Config, Runtime};
 
 pub struct SafePool {
-    pool: RwLock<Option<Pool>>,
+    pool: Option<Pool>,
     url: String,
 }
 
 impl SafePool {
     pub fn new(url: String) -> SafePool {
-        SafePool {
-            pool: RwLock::new(None),
-            url,
-        }
-    }
 
-    pub async fn invalidate(&self) {
-        let mut lock = self.pool.write().await;
-        *lock = None;
-    }
 
-    pub async fn ensure(&self) {
-        loop {
-            debug!("Trying to connect to Redis...");
-            match create(&self.url).await {
-                Ok(pool) => {
-                    let mut locked = self.pool.write().await;
-                    *locked = Some(pool);
-                    debug!("Connected to Redis.");
-                    break;
-                },
-                Err(e) => {
-                    error!("Failed to connect to Redis: {:?}", e);
-                    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await; // Wait before retrying
+        match create(&url) {
+            Ok(pool) => {
+
+                debug!("Connected to Redis.");
+
+                SafePool {
+                    pool: Some(pool),
+                    url,
                 }
+            },
+            Err(e) => {
+                let e = format!("Failed to connect to Redis: {:?}", e);
+                error!(e);
+                panic!("{}", e);
             }
         }
+
     }
 
+    pub async fn invalidate(&self) { //TODO
+        // let mut lock = self.pool.write().await;
+        // *lock = None;
+    }
+
+    #[instrument(skip(self))]
+    pub async fn ensure(&self) { // TODO
+        // loop {
+        //     debug!("Trying to connect to Redis...");
+        //     match create(&self.url) {
+        //         Ok(pool) => {
+        //             let mut locked = self.pool.write().await;
+        //             *locked = Some(pool);
+        //             debug!("Connected to Redis.");
+        //             break;
+        //         },
+        //         Err(e) => {
+        //             error!("Failed to connect to Redis: {:?}", e);
+        //             tokio::time::sleep(tokio::time::Duration::from_secs(1)).await; // Wait before retrying
+        //         }
+        //     }
+        // }
+    }
+
+    #[instrument(skip(self))]
     pub async fn get(&self) -> Result<Connection, Error> {
-        loop {
-            {
-                let lock = self.pool.read().await;
-                if let Some(pool) = &*lock {
-                    return Ok(pool.get().await?);
-                }
-            }
-            warn!("Redis connection lost, attempting to reconnect...");
-            self.ensure().await;
-        }
+        // loop {
+        //     {
+        //         let lock = self.pool.read().await;
+        //         if let Some(pool) = &*lock {
+        //             return Ok(pool.get().await?);
+        //         }
+        //     }
+        //     warn!("Redis connection lost, attempting to reconnect...");
+        //     self.ensure().await;
+        // }
+
+        Ok(self.pool.clone().unwrap().get().await?)
     }
 }
 
-async fn create(url: &str) -> Result<Pool, Error> {
+fn create(url: &str) -> Result<Pool, Error> {
     let cfg = Config::from_urls(vec![url.to_string()]);
     let pool = cfg.create_pool(Some(Runtime::Tokio1))?;
     Ok(pool)
