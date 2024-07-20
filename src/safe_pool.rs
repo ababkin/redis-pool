16855c99fedf8577f7a71fac4ecfdbf423337ab9
Alex Babkin <ababkin@Alexs-MacBook-Pro.local>
2024-07-20 12:08:35 +0200

    wip, pre parametrizing sharding

--- src/safe_pool.rs
+++ src/safe_pool.rs
@@ -1,5 +1,4 @@
 use anyhow::{Error, Result};
-use tokio::sync::RwLock;
 use tracing::{ debug, error, warn, instrument };
 use deadpool_redis::cluster::{Connection, Pool, Config, Runtime};
 
@@ -9,8 +8,6 @@ pub struct SafePool {
 
 impl SafePool {
     pub fn new(urls: Vec<String>) -> SafePool {
-
-
         match create(urls) {
             Ok(pool) => {
 
@@ -26,46 +23,10 @@ impl SafePool {
                 panic!("{}", e);
             }
         }
-
-    }
-
-    pub async fn invalidate(&self) { //TODO
-        // let mut lock = self.pool.write().await;
-        // *lock = None;
-    }
-
-    #[instrument(skip(self))]
-    pub async fn ensure(&self) { // TODO
-        // loop {
-        //     debug!("Trying to connect to Redis...");
-        //     match create(&self.url) {
-        //         Ok(pool) => {
-        //             let mut locked = self.pool.write().await;
-        //             *locked = Some(pool);
-        //             debug!("Connected to Redis.");
-        //             break;
-        //         },
-        //         Err(e) => {
-        //             error!("Failed to connect to Redis: {:?}", e);
-        //             tokio::time::sleep(tokio::time::Duration::from_secs(1)).await; // Wait before retrying
-        //         }
-        //     }
-        // }
     }
 
     #[instrument(name = "get_connection", skip(self))]
     pub async fn get(&self) -> Result<Connection, Error> {
-        // loop {
-        //     {
-        //         let lock = self.pool.read().await;
-        //         if let Some(pool) = &*lock {
-        //             return Ok(pool.get().await?);
-        //         }
-        //     }
-        //     warn!("Redis connection lost, attempting to reconnect...");
-        //     self.ensure().await;
-        // }
-
         Ok(self.pool.as_ref().unwrap().get().await?)
     }
 }
